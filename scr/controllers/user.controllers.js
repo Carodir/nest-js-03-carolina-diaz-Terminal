b3f40c55c083147d7505e704c07eaa95b8b32030
Carolina Diaz <c.diazrojas0905@gmail.com>
2025-09-13 23:31:01 -0500

    add : se modifica user.controllers.js en rama secundaria

--- scr/controllers/user.controllers.js
+++ scr/controllers/user.controllers.js
@@ -1,2 +1,4 @@
 /* comentarios explicativos, tengo duda sobre este tema 
-   asi que no pongo nada por ahora para despejar dudas en clase */
+   asi que no pongo nada por ahora para despejar dudas en clase */ 
+
+/* añadiendo esto para ver modificaciones en el archivo con git status */
